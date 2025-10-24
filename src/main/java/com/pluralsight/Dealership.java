ab3ce955af06f51250799b6bbf0da1a53c95a1fe
GoatGod-spec <dustindngo12@gmail.com>
2025-10-24 16:01:42 -0400

    m

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Dealership {
+
+    ArrayList<Vehicle> vehicles = new ArrayList<>();
+
+    public Dealership(){
+
+    }
+}
